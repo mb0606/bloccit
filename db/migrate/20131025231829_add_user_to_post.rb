c1ef2dc662aae813bd263a48b52f0b2e3cc7c56d
mb0606 <mberardini@evolution-man.com>
2013-10-27 12:15:58 -0700

    Typo in _add_user_to_path

--- db/migrate/20131025231829_add_user_to_post.rb
+++ db/migrate/20131025231829_add_user_to_post.rb
@@ -1,6 +1,6 @@
 class AddUserToPost < ActiveRecord::Migration
   def change
-    add_column :posts, :user_id, :interger
+    add_column :posts, :user_id, :integer
     add_index :posts, :user_id 
   end
 end
