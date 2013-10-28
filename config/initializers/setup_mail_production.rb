3c7d7f36ae0615bc690fbf63b12f483fa463f770
mb0606 <mberardini@evolution-man.com>
2013-10-27 18:07:39 -0700

    Replaced Sendgrid with Mandrill

--- config/initializers/setup_mail_production.rb
+++ config/initializers/setup_mail_production.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.mandrillapp.com',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['app18840800@heroku.com'],
-    :password       => ENV['im18divp'],
+    :password       => ENV['3ExjkEyQRGq4_vsoCRKWXQ'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
