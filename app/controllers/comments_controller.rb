105fdf64dec8ffa30166dd54f543c3554a42ef1e
mb0606 <mberardini@evolution-man.com>
2013-10-30 18:12:29 -0700

    Comments Part 3

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,7 +19,7 @@ class CommentsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(params[:comment])
-    @comment.post = @comment
+    @comment.post = @post
 
     authorize! :create, @comment, message: "You need to be signed up to do that."
     if @comment.save
