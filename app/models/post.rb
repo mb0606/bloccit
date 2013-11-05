103a33bd73edd13ad855ba8612575fa001b94edc
mb0606 <mberardini@evolution-man.com>
2013-11-05 13:07:17 -0800

    Implemented voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,13 @@ class Post < ActiveRecord::Base
 
   attr_accessible :body, :title, :topic, :image
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :topic
 
-  default_scope order('created_at DESC')
+  default_scope order('rank DESC')
+
+  after_create :create_vote
 
   mount_uploader :image, ImageUploader
 
@@ -14,6 +17,29 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true  
 
+  def up_votes
+    self.votes.where(value: 1).count
+  end
+
+  def down_votes
+    self.votes.where(value: -1).count
+  end 
+   
+  def points
+    self.votes.sum(:value).to_i
+  end  
+  def update_rank
+    age = (self.created_at - Time.new(1970,1,1)) / 86400
+    new_rank = points + age
+
+    self.update_attribute(:rank, new_rank)
+  end
+
+  private
+
+   def create_vote
+    user.votes.create(value: 1, post: self)
+  end  
 
 
 
